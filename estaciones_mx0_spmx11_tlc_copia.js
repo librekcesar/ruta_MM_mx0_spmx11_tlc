b744294410b520d1ce4f954473cc122942d0bb36
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 17:45:26 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx11_tlc_copia.js
@@ -0,0 +1,11 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx11_tlc",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.380693063674357, -99.28782993075704", "description": "SPMX11", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -99.2878332, 19.380694, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Blvd. Solidaridad las Torres 102, MZ 016, Las Arboledas, 52170 San Salvador Tizatlalli, Méx.", "description": "TLC1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.590949, 19.2764326, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Los Cerezos 922, MZ 013, Casa Blanca, 52175 San Jerónimo Chicahualco, Méx.", "description": "TLC2", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.6035292, 19.2793516, 0.0 ] } }
+]
+}
